632fa0ab2fc8652897a3068caaf32db8ff9a0270
Dhyan Soni <dhyantsoni@gmail.com>
2026-07-06 12:06:23 -0700

    fixing errors with width length + comments

--- mac.v
+++ mac.v
@@ -1,25 +1,24 @@
 module mac #(
     parameter DATA_WIDTH = 4
 ) (
-    input wire [0:0] s_i,
-    input wire [DATA_WIDTH-1:0] a_i,
-    input wire [DATA_WIDTH-1:0] b_i,
-    input wire [0:0] clk_i,
-    output [2*DATA_WIDTH-1:0] c_o
+    input wire [0:0] s_i,             // control signal
+    input wire [DATA_WIDTH-1:0] a_i,  // multiplier
+    input wire [DATA_WIDTH-1:0] b_i,  // multiplicant
+    input wire [0:0] clk_i,           // clock signal
+    output [2*DATA_WIDTH-1:0] c_o     // output product
 );
-    wire [3:0] intermediate_c [DATA_WIDTH-1:0]
+    wire [2*DATA_WIDTH-1:0] intermediate_c [DATA_WIDTH-1:0];
     genvar i; 
     generate
-        for (i = 0; i < DATA_WIDTH; i = i + 1) begin : add_loop
-            assign intermediate_c[i] = a_i[i] ? ({{DATA_WIDTH{1'b0}}, b_i}) : {2*DATA_WIDTH{1'b0}}
+        for (g = 0; g < DATA_WIDTH; g = g + 1) begin : add_loop
+            assign intermediate_c[g] = a_i[g] ? ({{DATA_WIDTH{1'b0}}, b_i}) : {2*DATA_WIDTH{1'b0}}
         end
     endgenerate
 
     integer i;
-
     always @(*) begin
-        c_o = DATA_WIDTH{1'b0};
-        for (i = 0; i < 8; i = i + 1) begin
+        c_o = {(2*DATA_WIDTH){1'b0}};
+        for (i = 0; i < DATA_WIDTH; i = i + 1) begin
             c_o = c_o + intermediate_c[i];
         end
     end
